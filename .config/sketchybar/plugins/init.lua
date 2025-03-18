e12cd4ba13559e7000ce14ddbe3cf9d3a9ae3a3a
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-03-18 18:19:49 +0200

    edited init.lua to load new widget and use new order

--- .config/sketchybar/plugins/init.lua
+++ .config/sketchybar/plugins/init.lua
@@ -1,14 +1,17 @@
 -- Left side
 require("plugins.apple")
--- require("plugins.app_spaces")
-require("plugins.front_app")
+require("plugins.app_spaces")
+require("plugins.weather")
+require("plugins.now_playing")
 
 -- Right side
 require("plugins.clock")
 require("plugins.battery")
 require("plugins.wifi")
 -- require("plugins.netstat")
-require("plugins.weather")
+require("plugins.outdated")
+require("plugins.cpu")
+require("plugins.cpu_temp")
 
 -- Center
-require("plugins.now_playing")
+require("plugins.front_app")
