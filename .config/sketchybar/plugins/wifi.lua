4da71d89c96d3fd02b81916fa9743cb381e2944e
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-04-28 10:01:17 +0300

    fixed hover race condition issue in the wifi widget

--- .config/sketchybar/plugins/wifi.lua
+++ .config/sketchybar/plugins/wifi.lua
@@ -1,4 +1,5 @@
 local settings = require("settings")
+local isHovered = false
 local wifi = sbar.add("item", {
 	position = "right",
 	label = {
@@ -12,12 +13,18 @@ local wifi = sbar.add("item", {
 })
 
 local function update(res)
+	local icon_color = settings.default_colors.icons.idle
+	if not isHovered then
+		icon_color = res == "" and settings.default_colors.icons.critical or settings.default_colors.icons.idle
+	else
+		icon_color = settings.default_colors.icons.hover
+	end
 	wifi:set({
 		label = {
 			string = res == "" and "Nicht verbunden" or res,
 		},
 		icon = {
-			color = res == "" and settings.default_colors.icons.critical or settings.default_colors.icons.idle,
+			color = icon_color,
 		},
 	})
 end
@@ -32,6 +39,7 @@ wifi:subscribe("routine", function()
 end)
 
 wifi:subscribe("mouse.entered", function()
+	isHovered = true
 	wifi:set({
 		background = {
 			drawing = "on",
@@ -47,6 +55,7 @@ wifi:subscribe("mouse.entered", function()
 end)
 
 wifi:subscribe("mouse.exited", function()
+	isHovered = false
 	wifi:set({
 		background = {
 			drawing = "off",
@@ -58,6 +67,12 @@ wifi:subscribe("mouse.exited", function()
 			color = settings.default_colors.icons.idle,
 		},
 	})
+	sbar.exec(
+		[[ipconfig getsummary "$(networksetup -listallhardwareports | awk '/Wi-Fi|AirPort/{getline; print $NF}')" | grep '  SSID : ' | awk -F ': ' '{print $2}']],
+		function(res)
+			update(res)
+		end
+	)
 end)
 
 wifi:subscribe("mouse.clicked", function()
