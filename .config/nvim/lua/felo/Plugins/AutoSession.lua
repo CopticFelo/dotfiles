0d24c613d2ed21c7e0705e47b3712d3fffb1bed1
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-05-07 15:37:54 +0300

    fixed Autosession

--- .config/nvim/lua/felo/Plugins/AutoSession.lua
+++ .config/nvim/lua/felo/Plugins/AutoSession.lua
@@ -1,12 +1,14 @@
 return {
-	"rmagatti/auto-session",
-	dependencies = {
-		"nvim-telescope/telescope.nvim", -- Only needed if you want to use sesssion lens
-	},
-	config = function()
-		require("auto-session").setup({
-			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-			auto_save_enabled = true,
-		})
-	end,
+  "rmagatti/auto-session",
+  dependencies = {
+    "nvim-telescope/telescope.nvim", -- Only needed if you want to use sesssion lens
+  },
+  config = function()
+    require("auto-session").setup({
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      auto_save = true,
+      auto_restore = true,
+      auto_restore_last_session = true,
+    })
+  end,
 }
