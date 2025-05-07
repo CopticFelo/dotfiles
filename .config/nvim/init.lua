c99811712783421d8f1ff593ae85692c867a395e
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-05-07 15:33:28 +0300

    changed locale to de_DE

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,10 +5,11 @@ require("nvim-tree").setup()
 
 vim.cmd("autocmd CmdlineLeave * redrawstatus")
 -- vim.api.nvim_exec ('language en_US', true)
-vim.cmd([[language en_US.UTF-8]])
+vim.cmd([[language de_DE.UTF-8]])
 vim.api.nvim_create_augroup("TerminalResize", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
-	group = "TerminalResize",
-	command = "resize 15",
+  group = "TerminalResize",
+  command = "resize 15",
 })
+
 vim.cmd("colorscheme oxocarbon")
