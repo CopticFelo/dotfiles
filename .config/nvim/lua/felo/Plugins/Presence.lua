211f55f9f81aa23cd3a2f801831b96d58b3d2bd1
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-06-01 13:45:16 +0300

    added Presence.nvim
    probably replacing with Cord later

---
+++ .config/nvim/lua/felo/Plugins/Presence.lua
@@ -0,0 +1,14 @@
+return {
+  "andweeb/presence.nvim",
+  opts = {
+    neovim_image_text   = "Der echte Code-Editor",
+
+    editing_text        = "%s bearbeiten",
+    file_explorer_text  = "%s durchsuchen",
+    git_commit_text     = "Änderungen committen",
+    plugin_manager_text = "Plugins verwalten",
+    reading_text        = "%s lesen",
+    workspace_text      = "An %s arbeiten",
+    line_number_text    = "Zeile %s von %s",
+  }
+}
