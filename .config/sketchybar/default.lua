e12d85805e604989d4dbd36cad8bf9214198dc83
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-06-01 13:33:40 +0300

    minor font and paddings adjustments

--- .config/sketchybar/default.lua
+++ .config/sketchybar/default.lua
@@ -2,33 +2,32 @@ local settings = require("settings")
 local colors = require("colors")
 
 sbar.default({
-	padding_left = 8,
-	padding_right = 8,
-	icon = {
-		font = {
-			family = settings.font,
-			style = "Bold",
-			size = 15,
-		},
-		color = settings.default_colors.icons.idle,
-		padding_left = settings.paddings,
-		padding_right = settings.paddings - 4,
-	},
-	label = {
-		font = {
-			family = settings.font,
-			-- style = "Bold",
-			size = 15.0,
-		},
-		color = settings.default_colors.text.idle,
-		padding_left = settings.paddings - 4,
-		padding_right = settings.paddings,
-	},
-	background = {
-		height = 24,
-		corner_radius = 15,
-		-- border_color = colors.white,
-		-- border_width = 1,
-		-- color = colors.white,
-	},
+  padding_left = settings.paddings.item_padding,
+  padding_right = settings.paddings.item_padding,
+  icon = {
+    font = {
+      family = settings.font,
+      style = "Bold",
+      size = 15,
+    },
+    color = settings.default_colors.icons.idle,
+    padding_left = settings.paddings.outer,
+    padding_right = settings.paddings.inner,
+  },
+  label = {
+    font = {
+      family = settings.font,
+      -- style = "Bold",
+      size = 15.0,
+    },
+    color = settings.default_colors.text.idle,
+    padding_right = settings.paddings.outer,
+  },
+  background = {
+    height = 24,
+    corner_radius = 15,
+    -- border_color = colors.white,
+    -- border_width = 1,
+    -- color = colors.white,
+  },
 })
--- .config/sketchybar/settings.lua
+++ .config/sketchybar/settings.lua
@@ -1,21 +1,25 @@
 local colors = require("colors")
 return {
-	font = "JetbrainsMono Nerd Font",
-	paddings = 6,
-	default_colors = {
-		hover = colors.white,
-		icons = {
-			idle = colors.blue,
-			warn = colors.blue,
-			critical = colors.red,
-			selected = colors.blue,
-			hover = colors.black,
-		},
-		text = {
-			idle = colors.white,
-			critical = colors.red,
-			selected = colors.red,
-			hover = colors.black,
-		},
-	},
+  font = "Arimo Nerd Font",
+  paddings = {
+    inner = 7,
+    outer = 5,
+    item_padding = 6
+  },
+  default_colors = {
+    hover = colors.white,
+    icons = {
+      idle = colors.blue,
+      warn = colors.blue,
+      critical = colors.red,
+      selected = colors.blue,
+      hover = colors.black,
+    },
+    text = {
+      idle = colors.white,
+      critical = colors.red,
+      selected = colors.red,
+      hover = colors.black,
+    },
+  },
 }
