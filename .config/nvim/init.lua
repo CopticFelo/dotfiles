939da54c82092d7d3eb7b4489f3fc825f86d0612
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-06-23 00:43:42 +0300

    moved most of init.lua code to options.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,31 +1,3 @@
+require("felo.lazy")
 require("felo.core.options")
 require("felo.core.keymaps")
-require("felo.lazy")
--- require("nvim-tree").setup()
-
-vim.cmd("autocmd CmdlineLeave * redrawstatus")
--- vim.api.nvim_exec ('language en_US', true)
-vim.cmd([[language de_DE.UTF-8]])
-vim.api.nvim_create_augroup("TerminalResize", { clear = true })
-vim.api.nvim_create_autocmd("TermOpen", {
-  group = "TerminalResize",
-  command = "resize 15",
-})
-
-vim.cmd("colorscheme gabriel")
-vim.o.guifont = "JetbrainsMono Nerd Font:h17"
-vim.g.neovide_opacity = 0.6
-vim.g.neovide_normal_opacity = 0.6
-vim.g.neovide_show_border = true
-vim.g.neovide_window_blurred = true
-vim.g.neovide_show_border = false
-vim.g.neovide_cursor_trail_size = 0.0
-vim.o.winborder = "single"
-vim.o.equalalways = false
-
-vim.lsp.enable({ "lua_ls", "asm_lsp", "basedpyright", "sourcekit" })
-vim.diagnostic.config({
-  virtual_lines = {
-    current_line = true,
-  },
-})
--- .config/nvim/lua/felo/core/options.lua
+++ .config/nvim/lua/felo/core/options.lua
@@ -1,6 +1,13 @@
 -- Appearance
 vim.opt.background = "dark"
 vim.opt.termguicolors = true
+vim.o.winborder = "single"
+vim.opt.title = true
+vim.opt.ttimeoutlen = 0
+vim.opt.wildmenu = true
+vim.o.guifont = "JetbrainsMono Nerd Font:h17"
+vim.cmd("colorscheme gabriel")
+vim.opt.guicursor = "n-v-c:block-Cursor,i-ci:block-iCursor"
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.completeopt = { "noinsert", "menuone", "noselect" }
@@ -15,6 +22,7 @@ vim.opt.relativenumber = true
 
 -- Split Windows
 vim.opt.splitright = true
+vim.o.equalalways = false
 
 -- Tab Settings
 vim.opt.expandtab = true
@@ -26,14 +34,32 @@ vim.opt.tabstop = 2
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+-- LSP
+vim.lsp.enable({ "lua_ls", "asm_lsp", "basedpyright", "sourcekit" })
+vim.diagnostic.config({
+  virtual_lines = {
+    current_line = true,
+  },
+})
+
+-- Neovide
+vim.g.neovide_opacity = 0.6
+vim.g.neovide_normal_opacity = 0.6
+vim.g.neovide_show_border = true
+vim.g.neovide_window_blurred = true
+vim.g.neovide_show_border = false
+vim.g.neovide_cursor_trail_size = 0.0
+
 -- Other
 vim.opt.wrap = false
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
 vim.cmd("highlight Cursor guifg=NONE guibg=#00D7FF")
 vim.cmd("highlight iCursor guifg=NONE guibg=#FF5F5F")
-vim.opt.guicursor = "n-v-c:block-Cursor,i-ci:block-iCursor"
-vim.opt.title = true
-vim.opt.ttimeoutlen = 0
-vim.opt.wildmenu = true
+vim.cmd("autocmd CmdlineLeave * redrawstatus")
+vim.cmd([[language de_DE.UTF-8]])
+vim.api.nvim_create_augroup("TerminalResize", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = "TerminalResize",
+  command = "resize 15",
+})
