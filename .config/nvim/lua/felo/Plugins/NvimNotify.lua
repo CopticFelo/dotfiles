32e94282046ef97a9321f37bdca986cf788417c1
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-11-15 12:21:11 +0200

    tweaked nvim_notify settings

--- .config/nvim/lua/felo/Plugins/NvimNotify.lua
+++ .config/nvim/lua/felo/Plugins/NvimNotify.lua
@@ -3,6 +3,10 @@ return {
   config = function()
     require("notify").setup({
       background_colour = "#000000",
+      timeout = 2,
+      max_height = 5,
+      fps = 60,
+      stages = "fade",
     })
   end,
 }
